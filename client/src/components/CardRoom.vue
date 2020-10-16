1ab12a4bc8ca45ffe0a387eefbc552a7095b643c
enricoanto <enricoanto@yahoo.com>
2020-10-16 12:55:04 +0700

    dev-client-revisi

--- client/src/components/CardRoom.vue
+++ client/src/components/CardRoom.vue
@@ -4,18 +4,18 @@
       <div class="col-3">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">score</h5>
-            <h1 class="display-1"> 0 </h1>
-            <h3 class="diplay-5"> player-name</h3>
+            <h5 class="card-title" style="font-family: 'Comic Sans MS', Comic Sans MS">score</h5>
+            <h1 class="display-1" style="font-family: 'Comic Sans MS', Comic Sans MS"> 0 </h1>
+            <h3 class="diplay-5" style="font-family: 'Comic Sans MS', Comic Sans MS"> player-name</h3>
           </div>
         </div>
       </div>
       <div class="col-3">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">score</h5>
-            <h1 class="display-1"> 0 </h1>
-            <h3 class="diplay-5"> player-name</h3>
+            <h5 class="card-title" style="font-family: 'Comic Sans MS', Comic Sans MS">score</h5>
+            <h1 class="display-1" style="font-family: 'Comic Sans MS', Comic Sans MS"> 0 </h1>
+            <h3 class="diplay-5" style="font-family: 'Comic Sans MS', Comic Sans MS"> player-name</h3>
           </div>
         </div>
       </div>
@@ -43,6 +43,11 @@
 
 <script>
 export default {
+  data () {
+    return {
+
+    }
+  }
 };
 </script>
 
